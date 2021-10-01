3afe058302a4c5288a5f1eacae6b33fb0aa5e1f1
yuseth <yusethnataly@gmail.com>
2021-10-01 11:37:57 -0400

    completando

---
+++ index.js
@@ -0,0 +1,31 @@
+import {getRandomInt} from './random.js'
+
+
+const createCardNode = ()=>{
+    const imagen = document.createElement("img")
+    // debugger
+    imagen.src=`https://source.unsplash.com/random/${getRandomInt()}` 
+    const imageContainer = document.createElement("div")
+    imageContainer.className = "image-container"
+    const parrafo = document.createElement("p")
+    parrafo.className = "description"
+    const text = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias perferendis ex, repudiandae dolorum illo illum itaque autem eveniet corporis et obcaecati est provident assumenda fuga aut debitis ab vel voluptas.')
+    parrafo.appendChild(text)
+    const card = document.createElement("div")
+    card.className="card"
+
+    imageContainer.appendChild(imagen)
+    card.appendChild(imageContainer)
+    card.appendChild(parrafo)
+
+    return card
+}
+
+const mountNode = document.querySelector("#app")
+const addCardButton = document.querySelector('button') 
+
+const addCard=()=>{
+    mountNode.appendChild(createCardNode())
+}
+
+add
---
+++ random.js
@@ -0,0 +1,5 @@
+const min = 0;
+const max = 1000;
+export function getRandomInt() {
+  return Math.floor(Math.random() * (max - min) + min);
+}
